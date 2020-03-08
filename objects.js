f885e759f47e1c3c6fe89e4920f48599eaee0e73
Brian Wong <brianw385@gmail.com>
2020-03-08 21:38:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { ok: 'what' };
 
-funtion updatePlaylist(playlist, ok, what) {
-  playlist[ok] = what;
+funtion updatePlaylist(playlist, 'key', 'value') {
+  playlist[key] = value;
   return playlist
 }
