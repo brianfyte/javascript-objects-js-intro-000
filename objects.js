a7fa1225835354ffaf677087c441cdbb3edb0beb
Brian Wong <brianw385@gmail.com>
2020-03-08 21:51:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist( playlist, ok, what) {
   playlist[ok] = 'what';
   return playlist
 }
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
